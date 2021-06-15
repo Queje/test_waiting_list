d0feaa39bce92324a52fd7e3c91cd4383c091d0d
Querne <jeremyquerne@hotmail.fr>
2021-06-15 15:51:10 +0200

    adding letter_opener in dev, usermailer and registration confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
-    enum waiting_status: { first_confirmation_needed: 0, confirmed: 1, reconfirmation_needed: 2 }
+  enum waiting_status: { first_confirmation_needed: 0, confirmed: 1, reconfirmation_needed: 2 }
 
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "is not a valid email" }
-    validates :phone, uniqueness: {case_sensitive: false, message: "is already taken" }, format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/, message: "is not a valid french number" }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "is not a valid email" }
+  validates :phone, uniqueness: {case_sensitive: false, message: "is already taken" }, format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/, message: "is not a valid french number" }
+
+  after_create :send_registration_confirmation
+
+  def send_registration_confirmation
+    UserMailer.registration_confirmation(self).deliver_now
+  end
 end
  
