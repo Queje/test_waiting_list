9cc548c9bdc5209bd40476cb06a995a3cb781598
Querne <jeremyquerne@hotmail.fr>
2021-06-16 11:34:54 +0200

    using an encrypted cookie to filter user to the confirmation/cancel view

--- app/controllers/waiting_list_controller.rb
+++ app/controllers/waiting_list_controller.rb
@@ -1,8 +1,9 @@
 class WaitingListController < ApplicationController
   before_action :set_user, only: %i[show update]
+  before_action :check_cookie, only: %i[show update]
 
   def show
-
+    
   end
 
   def update
@@ -18,6 +19,12 @@ class WaitingListController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def check_cookie
+    unless @user.email == cookies.encrypted[:email]
+      redirect_to root_path
+    end
   end
 
 end
